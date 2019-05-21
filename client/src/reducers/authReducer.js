e5eb75405ae0534a690fc4ef1694b216ad0dc075
amisha <cse.amishajain@gmail.com>
2019-05-21 09:27:33 -0700

    Redux action reducer and error reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,4 @@
+//import { TEST_DISPATCH } from "../actions/types";
 const initialState = {
   isAuthenticate: false,
 
@@ -6,6 +7,11 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    // case TEST_DISPATCH:
+    //   return {
+    //     ...state,
+    //     user: action.payload
+    //   };
     default:
       return state;
   }
